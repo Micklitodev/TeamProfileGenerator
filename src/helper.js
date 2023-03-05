4ed3117afec60df6707ae465fd04f94cfc191e04
[mick lito] <[micklito.dev@gmail.com]>
2023-03-05 12:24:37 -0500

    0.4.1
    fixed bug where a tag was not following the flow of the card by centering the test within the card. fixed html doc title to say, my team instead of my app.

--- src/helper.js
+++ src/helper.js
@@ -8,7 +8,7 @@ const writeHtml = (data) => {
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>My App</title>
+      <title>My Team</title>
       <link rel="stylesheet" href="./style.css" />
     </head>
     <body>
