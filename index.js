61d97e744364dc9149a0bfaf43f7f29a3b299c84
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-03-05 00:20:55 -0500

    0.3.0
    updated lib to have intern file that contains intern class and a manager file that contains manager class, added in dist folder that contains the css, and html, files, updated style.css to corectly center a tags as well. updated all lib files to have a type, this was done for the update on index.js, all types (company position) are selected by app function passed to decided function a conditional checks the type then passes it to the correct ql with type param, from there after the question line has run create object function will be called with data and the type variable we have been continuously passing, create object destructures the object param then a conditional checks if the new object to be created should be engineer, manager, or intern, from the type we have been passing. and then pushes that to an objects array then calls more emp if no is selected render data function is called render data takes the objectarr with the foreach method and uses fs.appendFile to fill each portion of the index.html, using the renderhtml function. which defines the html with template literals. when complete object destructuring was used on the forEach variable, to console log that the html card was sucessfully generated with the type of position that was created.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Engineer = require("./lib/engineer");
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
 
 const objectsArr = [];
 
 const renderHtml = (element) => {
-  const { name, certs, github, phone } = element;
+  const { type, name, certs, github, phone } = element;
 
   return `
 <!DOCTYPE html>
@@ -20,9 +22,10 @@ const renderHtml = (element) => {
 <body>
 
  <div class="card"> 
-  <h2> ${name} </h2>
+  <h2> ${type} </h2>
+  <h3> ${name} </h3>
   <h3> ${certs} </h3> 
-  <h3> ${github} </h3>
+  <a href='${github}' >  Github </a>
   <h3> ${phone} </h3>
 </div>
 
@@ -35,41 +38,94 @@ const renderData = () => {
   objectsArr.forEach((element) => {
     const fill = renderHtml(element);
 
-    fs.appendFile("index.html", fill, (err) => {
-      err ? console.log(err) : console.log("sucess");
+    const { type } = element;
+
+    fs.appendFile("./dist/index.html", fill, (err) => {
+      err ? console.log(err) : console.log(`Sucessfully made a ${type} card`);
     });
   });
 };
 
 const decide = ({ type }) => {
   if (type === "engineer") {
-    qlForEng();
+    qlForEng(type);
   } else if (type === "manager") {
-    qlForMgmt();
+    qlForMgmt(type);
+  } else if (type === 'intern') {
+    qlForIntern(type)
   }
 };
 
-const createObject = ({ certs, name }) => {
-  objectsArr.push(new Engineer(certs, name));
+const createObject = ({ certs, name }, type ) => {
+  if(type === 'engineer') {
+  objectsArr.push(new Engineer(type, certs, name));
+  console.log('en')
+  }
+  if (type === 'manager') {
+    objectsArr.push(new Manager(type, certs, name))
+    console.log('mg')
+  }
+  if (type === 'intern') {
+    objectsArr.push(new Intern(type, certs, name))
+    console.log('it')
+  }
   moreEmp();
 };
 
-const qlForEng = () => {
+const qlForEng = (type) => {
   inquirer
     .prompt([
-      {
-        type: "input",
-        message: "What certifications do you have?",
-        name: "certs",
-      },
       {
         type: "input",
         message: "What is your name? ",
         name: "name",
       },
+      {
+        type: "input",
+        message: "What certifications do you have?",
+        name: "certs",
+      },
     ])
     .then((data) => {
-      createObject(data);
+      createObject(data, type);
+    });
+};
+
+const qlForMgmt = (type) => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name? ",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What certifications do you have?",
+        name: "certs",
+      },
+    ])
+    .then((data) => {
+      createObject(data, type);
+    });
+};
+
+const qlForIntern = (type) => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name? ",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What certifications do you have?",
+        name: "certs",
+      },
+    ])
+    .then((data) => {
+      createObject(data, type);
     });
 };
 
@@ -80,7 +136,7 @@ const app = () => {
         type: "list",
         message: "What type of employee is this?",
         name: "type",
-        choices: ["manager", "engineer"],
+        choices: ["manager", "engineer", "intern" ],
       },
     ])
     .then((data) => {
